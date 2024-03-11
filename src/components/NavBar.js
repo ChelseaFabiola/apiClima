4dc0ebd7328ae42e19554cf16be83149b13142ab
ChelseaFabiola <161035433+ChelseaFabiola@users.noreply.github.com>
2024-03-11 00:27:12 -0500

    Primer commint

---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+const NavBar = () => {
+    return(
+
+        <nav className="navbar bg-dark text-light mb-5">
+            <div className="container-fluid">
+                <h3 className="mx-auto">Predicción Meteorológica</h3>
+            </div>
+            <div className="container-fluid">
+                <h5 className="mx-auto">Chelsea Fabiola Hernández Ceja</h5>
+            </div>
+        </nav>
+
+    );
+}
+
+export default NavBar;
